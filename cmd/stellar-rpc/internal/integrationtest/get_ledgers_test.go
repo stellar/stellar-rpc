534e6aa4ac9340919ad6ea38c3ab3f1905c5c87a
Urvi <urvi.savla@stellar.org>
2025-05-26 21:42:02 -0700

    Add integration test for GetLedgers using fake GCS server

--- cmd/stellar-rpc/internal/integrationtest/get_ledgers_test.go
+++ cmd/stellar-rpc/internal/integrationtest/get_ledgers_test.go
@@ -1,20 +1,26 @@
 package integrationtest
 
 import (
-	"context"
+	"bytes"
+	"fmt"
 	"testing"
+	"time"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/fsouza/fake-gcs-server/fakestorage"
 	"github.com/stretchr/testify/require"
 
+	"github.com/stellar/go/ingest/ledgerbackend"
+	"github.com/stellar/go/support/compressxdr"
+	"github.com/stellar/go/support/datastore"
+	"github.com/stellar/go/xdr"
+
+	"github.com/stellar/stellar-rpc/client"
+	"github.com/stellar/stellar-rpc/cmd/stellar-rpc/internal/config"
 	"github.com/stellar/stellar-rpc/cmd/stellar-rpc/internal/integrationtest/infrastructure"
 	"github.com/stellar/stellar-rpc/protocol"
 )
 
-func TestGetLedgers(t *testing.T) {
-	test := infrastructure.NewTest(t, nil)
-	client := test.GetRPCLient()
-
+func testGetLedgers(t *testing.T, client *client.Client) {
 	// Get all ledgers
 	request := protocol.GetLedgersRequest{
 		StartLedger: 8,
@@ -23,9 +29,9 @@ func TestGetLedgers(t *testing.T) {
 		},
 	}
 
-	result, err := client.GetLedgers(context.Background(), request)
+	result, err := client.GetLedgers(t.Context(), request)
 	require.NoError(t, err)
-	assert.Len(t, result.Ledgers, 3)
+	require.Len(t, result.Ledgers, 3)
 	prevLedgers := result.Ledgers
 
 	// Get ledgers using previous result's cursor
@@ -35,10 +41,10 @@ func TestGetLedgers(t *testing.T) {
 			Limit:  2,
 		},
 	}
-	result, err = client.GetLedgers(context.Background(), request)
+	result, err = client.GetLedgers(t.Context(), request)
 	require.NoError(t, err)
-	assert.Len(t, result.Ledgers, 2)
-	assert.Equal(t, prevLedgers[len(prevLedgers)-1].Sequence+1, result.Ledgers[0].Sequence)
+	require.Len(t, result.Ledgers, 2)
+	require.Equal(t, prevLedgers[len(prevLedgers)-1].Sequence+1, result.Ledgers[0].Sequence)
 
 	// Test with JSON format
 	request = protocol.GetLedgersRequest{
@@ -48,10 +54,10 @@ func TestGetLedgers(t *testing.T) {
 		},
 		Format: protocol.FormatJSON,
 	}
-	result, err = client.GetLedgers(context.Background(), request)
+	result, err = client.GetLedgers(t.Context(), request)
 	require.NoError(t, err)
-	assert.NotEmpty(t, result.Ledgers[0].LedgerHeaderJSON)
-	assert.NotEmpty(t, result.Ledgers[0].LedgerMetadataJSON)
+	require.NotEmpty(t, result.Ledgers[0].LedgerHeaderJSON)
+	require.NotEmpty(t, result.Ledgers[0].LedgerMetadataJSON)
 
 	// Test invalid requests
 	invalidRequests := []protocol.GetLedgersRequest{
@@ -65,7 +71,97 @@ func TestGetLedgers(t *testing.T) {
 	}
 
 	for _, req := range invalidRequests {
-		_, err = client.GetLedgers(context.Background(), req)
-		assert.Error(t, err)
+		_, err = client.GetLedgers(t.Context(), req)
+		require.Error(t, err)
 	}
+}
+
+func TestGetLedgers(t *testing.T) {
+	test := infrastructure.NewTest(t, nil)
+	client := test.GetRPCLient()
+	testGetLedgers(t, client)
+}
+
+func TestGetLedgersFromDatastore(t *testing.T) {
+	// setup fake GCS server
+	opts := fakestorage.Options{
+		Scheme:     "http",
+		PublicHost: "127.0.0.1",
+	}
+	gcsServer, err := fakestorage.NewServerWithOptions(opts)
+	require.NoError(t, err)
+	defer gcsServer.Stop()
+
+	t.Setenv("STORAGE_EMULATOR_HOST", gcsServer.URL())
+	bucketName := "test-bucket"
+	gcsServer.CreateBucketWithOpts(fakestorage.CreateBucketOpts{Name: bucketName})
+
+	// add files to GCS
+	for i, seq := range []uint32{8, 9, 10} {
+		objectName := fmt.Sprintf("FFFFFFF%d--%d.xdr.zstd", 7-i, seq)
+		gcsServer.CreateObject(fakestorage.Object{
+			ObjectAttrs: fakestorage.ObjectAttrs{
+				BucketName: bucketName,
+				Name:       objectName,
+			},
+			Content: createLCMBatchBuffer(seq),
+		})
+	}
+
+	// datastore configuration function
+	setDatastoreConfig := func(cfg *config.Config) {
+		cfg.ServeLedgersFromDatastore = true
+		cfg.BufferedStorageBackendConfig = ledgerbackend.BufferedStorageBackendConfig{
+			BufferSize: 10,
+			NumWorkers: 2,
+			RetryLimit: 3,
+			RetryWait:  30 * time.Second,
+		}
+		cfg.DataStoreConfig = datastore.DataStoreConfig{
+			Type:   "GCS",
+			Params: map[string]string{"destination_bucket_path": bucketName},
+			Schema: datastore.DataStoreSchema{
+				FilesPerPartition: 1,
+				LedgersPerFile:    1,
+			},
+		}
+		// reduce retention windows to force usage of datastore
+		cfg.HistoryRetentionWindow = 10
+		cfg.ClassicFeeStatsLedgerRetentionWindow = 10
+		cfg.SorobanFeeStatsLedgerRetentionWindow = 10
+	}
+
+	test := infrastructure.NewTest(t, &infrastructure.TestConfig{
+		NoParallel:          true, // can't use parallel due to env vars
+		DatastoreConfigFunc: setDatastoreConfig,
+	})
+	client := test.GetRPCLient()
+
+	// run tests
+	testGetLedgers(t, client)
+}
+
+func createLCMBatchBuffer(seq uint32) []byte {
+	lcm := xdr.LedgerCloseMetaBatch{
+		StartSequence: xdr.Uint32(seq),
+		EndSequence:   xdr.Uint32(seq),
+		LedgerCloseMetas: []xdr.LedgerCloseMeta{
+			{
+				V: int32(0),
+				V0: &xdr.LedgerCloseMetaV0{
+					LedgerHeader: xdr.LedgerHeaderHistoryEntry{
+						Header: xdr.LedgerHeader{
+							LedgerSeq: xdr.Uint32(seq),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	var buf bytes.Buffer
+	encoder := compressxdr.NewXDREncoder(compressxdr.DefaultCompressor, lcm)
+	_, _ = encoder.WriteTo(&buf)
+
+	return buf.Bytes()
 }
