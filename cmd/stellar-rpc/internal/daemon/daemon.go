c02fa9a6b9ad6bfb442326c8aa77dfa3ad961940
George <george@stellar.org>
2025-07-15 13:28:29 -0700

    Turn off MetaV4 for the time being

--- cmd/stellar-rpc/internal/daemon/daemon.go
+++ cmd/stellar-rpc/internal/daemon/daemon.go
@@ -148,7 +148,7 @@ func newCaptiveCore(cfg *config.Config, logger *supportlog.Entry) (*ledgerbacken
 		Strict:                             true,
 		EnforceSorobanDiagnosticEvents:     true,
 		EnforceSorobanTransactionMetaExtV1: true,
-		EmitUnifiedEvents:                  true,
+		EmitUnifiedEvents:                  false,
 		CoreBinaryPath:                     cfg.StellarCoreBinaryPath,
 		HTTPQueryServerParams:              queryServerParams,
 	}
