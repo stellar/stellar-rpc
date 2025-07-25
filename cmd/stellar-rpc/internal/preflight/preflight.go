6a95232d8ccfde9327f93cb3971c3752de9ba45d
George <george@stellar.org>
2025-07-25 11:15:57 -0700

    Group defers to show proper deallocs

--- cmd/stellar-rpc/internal/preflight/preflight.go
+++ cmd/stellar-rpc/internal/preflight/preflight.go
@@ -268,7 +268,9 @@ func getInvokeHostFunctionPreflight(ctx context.Context, params Parameters) (Pre
 		C.uint32_t(authMode),
 	)
 
-	return GoPreflight(res), nil
+	preflightRes := GoPreflight(res)
+
+	return preflightRes, nil
 }
 
 func GoPreflight(result *C.preflight_result_t) Preflight {
