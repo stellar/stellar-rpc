b43a75c9de2cc98c8ffdfffa5035bfc3bf040848
George <george@stellar.org>
2025-08-28 11:53:53 -0700

    Remove extra {scope}

--- cmd/stellar-rpc/internal/xdr2json/conversion.go
+++ cmd/stellar-rpc/internal/xdr2json/conversion.go
@@ -60,20 +60,17 @@ func ConvertInterface(xdr encoding.BinaryMarshaler) (json.RawMessage, error) {
 
 func convertAnyBytes(xdrTypeName string, field []byte) (json.RawMessage, error) {
 	var jsonStr, errStr string
-	// scope just added to show matching alloc/frees
-	{
-		goRawXdr := CXDR(field)
-		defer FreeGoXDR(goRawXdr)
+	goRawXdr := CXDR(field)
+	defer FreeGoXDR(goRawXdr)
 
-		b := C.CString(xdrTypeName)
-		defer C.free(unsafe.Pointer(b))
+	b := C.CString(xdrTypeName)
+	defer C.free(unsafe.Pointer(b))
 
-		result := C.xdr_to_json(b, goRawXdr)
-		defer C.free_conversion_result(result)
+	result := C.xdr_to_json(b, goRawXdr)
+	defer C.free_conversion_result(result)
 
-		jsonStr = C.GoString(result.json)
-		errStr = C.GoString(result.error)
-	}
+	jsonStr = C.GoString(result.json)
+	errStr = C.GoString(result.error)
 
 	if errStr != "" {
 		return json.RawMessage(jsonStr), errors.New(errStr)
