6016edbb04c2170a097dd90c87e34ceb3963f354
Christian Jonas <christianjonas99@gmail.com>
2025-12-03 03:58:12 -0500

    updated getLatestLedger and testing in protocol-25

--- cmd/stellar-rpc/internal/methods/get_latest_ledger.go
+++ cmd/stellar-rpc/internal/methods/get_latest_ledger.go
@@ -2,6 +2,8 @@ package methods
 
 import (
 	"context"
+	"encoding/base64"
+	"fmt"
 
 	"github.com/creachadair/jrpc2"
 
@@ -20,7 +22,6 @@ func NewGetLatestLedgerHandler(ledgerReader db.LedgerReader) jrpc2.Handler {
 				Message: "could not get latest ledger sequence",
 			}
 		}
-
 		latestLedger, found, err := ledgerReader.GetLedger(ctx, latestSequence)
 		if (err != nil) || (!found) {
 			return protocol.GetLatestLedgerResponse{}, &jrpc2.Error{
@@ -28,12 +29,31 @@ func NewGetLatestLedgerHandler(ledgerReader db.LedgerReader) jrpc2.Handler {
 				Message: "could not get latest ledger",
 			}
 		}
-
+		header := latestLedger.LedgerHeaderHistoryEntry().Header
+		headerBytes, err := header.MarshalBinary()
+		if err != nil {
+			return protocol.GetLatestLedgerResponse{}, &jrpc2.Error{
+				Code:    jrpc2.InternalError,
+				Message: fmt.Sprintf("could not marshal latest ledger header: %v", err),
+			}
+		}
 		response := protocol.GetLatestLedgerResponse{
 			Hash:            latestLedger.LedgerHash().HexString(),
 			ProtocolVersion: latestLedger.ProtocolVersion(),
 			Sequence:        latestSequence,
+			LedgerCloseTime: latestLedger.LedgerCloseTime(),
+			LedgerHeader:    base64.StdEncoding.EncodeToString(headerBytes),
 		}
+
+		raw, err := latestLedger.MarshalBinary()
+		if err != nil {
+			return protocol.GetLatestLedgerResponse{}, &jrpc2.Error{
+				Code:    jrpc2.InternalError,
+				Message: fmt.Sprintf("could not marshal latest ledger metadata: %v", err),
+			}
+		}
+		response.LedgerMetadata = base64.StdEncoding.EncodeToString(raw)
+
 		return response, nil
 	})
 }
