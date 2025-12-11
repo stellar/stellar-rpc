7f785c1a91d8604460045e472a5025d842a7ec2f
Christian Jonas <christianjonas99@gmail.com>
2025-12-11 11:46:10 -0500

    minor linter

--- cmd/stellar-rpc/internal/methods/get_network.go
+++ cmd/stellar-rpc/internal/methods/get_network.go
@@ -12,6 +12,7 @@ import (
 
 	protocol "github.com/stellar/go-stellar-sdk/protocols/rpc"
 	"github.com/stellar/go-stellar-sdk/support/log"
+
 	"github.com/stellar/stellar-rpc/cmd/stellar-rpc/internal/db"
 )
 
