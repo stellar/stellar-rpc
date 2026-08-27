bea48afa032159c71f0548bb7eb129cd8c0028f5
Christian Jonas <christianjonas99@gmail.com>
2026-08-27 16:13:18 -0400

    update mocks

--- cmd/stellar-rpc/internal/methods/mocks.go
+++ cmd/stellar-rpc/internal/methods/mocks.go
@@ -76,6 +76,13 @@ func (m *MockLedgerReaderTx) GetLedger(ctx context.Context, sequence uint32) (xd
 	return args.Get(0).(xdr.LedgerCloseMeta), args.Bool(1), args.Error(2) //nolint:forcetypeassert
 }
 
+func (m *MockLedgerReaderTx) GetLedgerView(ctx context.Context, sequence uint32) (
+	xdr.LedgerCloseMetaView, bool, error,
+) {
+	args := m.Called(ctx, sequence)
+	return args.Get(0).(xdr.LedgerCloseMetaView), args.Bool(1), args.Error(2) //nolint:forcetypeassert
+}
+
 func (m *MockLedgerReaderTx) Done() error {
 	args := m.Called()
 	return args.Error(0)
