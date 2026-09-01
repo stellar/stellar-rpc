026974da885b2942b1d47ff5a056db500bbbd931
Christian Jonas <christianjonas99@gmail.com>
2026-09-01 13:57:20 -0400

    bump SDK + remove InSuccessfulContractCall from getEventsV1 protocol struct

--- cmd/stellar-rpc/internal/rpcv1/sqlitedb/transaction.go
+++ cmd/stellar-rpc/internal/rpcv1/sqlitedb/transaction.go
@@ -197,7 +197,7 @@ func (txn *transactionHandler) getTransactionByHash(ctx context.Context, hash xd
 		return store.Transaction{}, store.ErrNoTransaction
 	}
 	txView := txnViewRange[0]
-	return store.ParseTransaction(txView), nil
+	return store.ParseTransactionView(txView), nil
 }
 
 type transactionTableMigration struct {
