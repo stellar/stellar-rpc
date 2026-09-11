14814e4e71d594249e3d3854ec1c6756b13474e3
Christian Jonas <christianjonas99@gmail.com>
2026-09-11 13:21:22 -0400

    set cursor before early return with TransactionOrder at 0

--- cmd/stellar-rpc/internal/methods/get_transactions.go
+++ cmd/stellar-rpc/internal/methods/get_transactions.go
@@ -131,7 +131,7 @@ func (h transactionsRPCHandler) processTransactionsInLedger(
 	if ledgerSeqInt32 == start.LedgerSequence {
 		startTxIdx = int(start.TransactionOrder)
 	}
-	cursor := toid.New(ledgerSeqInt32, 0, 1)
+	cursor := toid.New(ledgerSeqInt32, int32(startTxIdx-1), 1)
 	remaining := limitInt - len(*txns)
 	if remaining <= 0 {
 		return cursor, true, nil
